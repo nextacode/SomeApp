7f2693a73c74b4f2db2b3f2e4b828cb8a89101fb
nextacode <151906636+nextacode@users.noreply.github.com>
2023-11-24 08:24:20 +0100

    clerk

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,21 @@
-import './globals.css'
-
+import {ClerkProvider} from '@clerk/nextjs'
 import {GeistMono} from 'geist/font/mono'
+
+import './globals.css'
 
 export const metadata = {
 	title: 'SomeApp',
 	description: ''
 }
 
+// export const preferredRegion = 'iad1'
+
 export default function RootLayout ({children}) {
-	return <html lang='en'>
-		<body className={GeistMono.className}>
-			{children}
-		</body>
-	</html>
+	return <ClerkProvider>
+		<html lang='en'>
+			<body className={GeistMono.className}>
+				{children}
+			</body>
+		</html>
+	</ClerkProvider>
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,23 @@
-export default function Homepage() {
-	return <div className='text-[30px]'>
-		Hello World
-	</div>
+import {UserButton, auth, currentUser} from '@clerk/nextjs'
+import Link from 'next/link'
+
+export default async function Homepage() {
+	const {userId} = auth()
+	const user = await currentUser()
+
+	return <>
+		<div className='text-[30px] font-bold mb-5'>
+			SomeApp
+		</div>
+
+		{
+			userId ? <>
+				Welcome {user.username}!
+
+				<UserButton afterSignOutUrl='/'/>
+			</> : <Link href='/signin' className='bg-[#9147ff] p-2 rounded text-[20px] font-semibold'>
+				Signin
+			</Link>
+		}
+	</>
 }
